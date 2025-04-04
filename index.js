00784b4f2810d5218ee3387e720401ed2e867210
Chris <cldarling@gmail.com>
2025-04-03 21:33:07 -0400

    at 21:00 give or take

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function showUserPosts(id) {
 function userHTML(user) {
   return `<div class="user-card" onclick="showUserPosts(${user.id})">
     <div class="user-card__container">
-      <h3>${user.name}</h4>
+      <h3>${user.name}</h3>
         <p><b>Email:</b> ${user.email}</p>
         <p><b>Phone:</b> ${user.phone}</p>
         <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
--- user.js
+++ user.js
@@ -1,9 +1,41 @@
+const postListEl = document.querySelector(".post-list")
+
+async function main(event) {
+    const id = event.target.value;
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map(post => 
+        `<div class="post-list">
+    <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+  </div>`
+    ).join("");
+}
+
+main();
+
 async function main() {
     const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-
-    console.log(postsData)
+    postListEl.innerHTML = postsData.map(post => 
+        `<div class="post-list">
+    <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+  </div>`
+    ).join("");
 }
 
 main();
